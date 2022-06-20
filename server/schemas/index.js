6b4483c009b051b250ef54c828b2d73eb6a50943
Chris <christopher.sarm15@gmail.com>
2022-06-20 13:00:40 -0400

    created typeDefs for gql

---
+++ server/schemas/index.js
@@ -0,0 +1,64 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Category {
+    _id: ID
+    name: String
+  }
+
+  type Product {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    quantity: Int
+    price: Float
+    category: Category
+    reviews: [Review]
+  }
+
+  type Order {
+    _id: ID
+    rentalDate: String
+    products: [Product]
+  }
+
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+  }
+
+  type Review {
+    _id: ID
+    reviewBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    categories: [Category]
+    products(category: ID, name: String): [Product]
+    product(_id: ID!): Product
+    user: User
+    order(_id: ID!): Order
+  }
+
+  type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addOrder(products: [ID]!): Order
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    updateProduct(_id: ID!, quantity: Int!): Product
+    addReview(productId: ID!, reviewBody: String!): Product
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;

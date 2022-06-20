2350cfb91079ce799201eb4cb8f05eb1def15131
Chris <christopher.sarm15@gmail.com>
2022-06-20 16:38:29 -0400

    added categorymenu component

---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
+import Cart from '../components/Cart';
+
+const Home = () => {
+  //console.log('we are testing')
+  return (
+    <div className="container">
+      
+      <CategoryMenu />
+      <ProductList />
+      <Cart />
+    </div>
+  );
+};
+
+export default Home;

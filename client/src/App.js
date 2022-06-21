0b47d8c767bf2fd319d86bb1fbd906cdad7d3438
Your Name <kalaitzidispaul@gmail.com>
2022-06-21 14:04:23 -0400

    added a proxy

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import {
   createHttpLink,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-
 import Detail from "./pages/Detail";
 import Home from "./pages/Home";
 import Login from "./pages/Login";

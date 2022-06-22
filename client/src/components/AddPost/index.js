923a96c9d9c6fb98681321739de89b73be5c1a97
Your Name <kalaitzidispaul@gmail.com>
2022-06-22 19:43:13 -0400

    added form

--- client/src/components/AddPost/index.js
+++ client/src/components/AddPost/index.js
@@ -1,37 +1,93 @@
-import React, {useState} from 'react'
-import Axios from 'axios'
+import React, { useState } from "react";
+import "./style.css";
 
 const AddPost = () => {
+  // const [formData, setFormData] = useState({
+  //   //input values in textarea
+  //   name: '',
+  //   price: '',
+  //   description: '',
+  //   image: ''
+  // });
 
-    const [imageSelected, setImageSelected] = useState("")
+  // const uploadImage = (files) => {
+  //     console.log(files[0])
+  //     const formData = new FormData()
+  //     formData.append('file', imageSelected)
+  //     formData.append('upload_preset', 'RentAFit')
 
-    const uploadImage = (files) => {
-        console.log(files[0])
-        const formData = new FormData()
-        formData.append('file', imageSelected)
-        formData.append('upload_preset', 'RentAFit')
-   
+  //     Axios.post('https://api.cloudinary.com/v1_1/rentafit/image/upload', FormData
+  //     ).then((response) => {
+  //         console.log(response)
+  //     })
+  // }
 
-        Axios.post('https://api.cloudinary.com/v1_1/rentafit/image/upload', FormData
-        ).then((response) => {
-            console.log(response)
-        })
-    }
+  // const handleImageUpload = async () => {
+  //     const data = new FormData()
+  //     data.append('file', image)
+  //     data.append('upload_preset', 'RentAFit')
+  //     data.append("cloud_name", "rentafit")
+  // }
 
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-    // const handleImageUpload = async () => {
-    //     const data = new FormData()
-    //     data.append('file', image)
-    //     data.append('upload_preset', 'RentAFit')
-    //     data.append("cloud_name", "rentafit")
-    // }
+    console.log("HEllo");
+  };
   return (
-    <div>
-    <input type='file' onChange={(event) =>{setImageSelected(event.target.files[0])}}/>
+    <div className="post_container">
+    <div className="form-container-parent">
 
-    <button onClick={uploadImage}>upload Post</button>
+      <form onSubmit={handleFormSubmit} className="form-container">
+
+      <span className="add-post-title">
+      <h1>Post a Product</h1>
+      </span>
+     
+
+      
+        <h3>Title</h3>
+        <div className="price-input-container">
+        <textarea
+        id="title-input"
+        // value={formData.name}
+        className="form-input"
+      ></textarea>
+          
+          <input
+            id="price-input"
+            // value={formData.price}
+            className="form-input "
+            type="number"
+            min="1"
+            max="1000"
+          ></input> 
+          <p className="per-day"> /day</p>
+
+        
+        </div>
+
+        <span>
+          <input
+            type="file"
+            //  value={formData.image}
+          ></input>
+        </span>
+
+        <h3>Description</h3>
+        <textarea
+          id="description-input"
+          //  value={formData.description}
+          className="form-input"
+        ></textarea>
+        <button type="submit" className="add-post-btn">
+        Add Post
+        </button>
+      </form>
+     
     </div>
-  )
-}
+    </div>
+  );
+};
 
-export default AddPost
+export default AddPost;

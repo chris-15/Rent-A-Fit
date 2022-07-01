2c2526e9d558c280a8a9c8edd6aa531f47d68ebb
Your Name <kalaitzidispaul@gmail.com>
2022-06-30 21:29:02 -0400

    added code

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  response.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 app.get('/', (req, res) => {
